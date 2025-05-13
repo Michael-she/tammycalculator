780280a2a4bb31f84cda6befef7f6d0e1f4caca2
Michael Shepstone <mgshepstone@gmail.com>
2025-05-13 18:57:27 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Tammy\'s Calculator',
+  description: 'You know exactly what this is.',
+  generator: 'AAAAAAAAAAAAAAAAAAAA',
 }
 
 export default function RootLayout({
